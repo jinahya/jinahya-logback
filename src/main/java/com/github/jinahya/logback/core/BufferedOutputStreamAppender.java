1c66a6594ea4bfe8c5477eeaf86cf32fc467290a
Jin Kwon <onacit@gmail.com>
2015-01-09 02:40:31 +0900

    BufferedOutputStreamAppender added

---
+++ src/main/java/com/github/jinahya/logback/core/BufferedOutputStreamAppender.java
@@ -0,0 +1,242 @@
+/*
+ * Copyright 2015 Jin Kwon &lt;jinahya_at_gmail.com&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.github.jinahya.logback.core;
+
+
+import ch.qos.logback.core.OutputStreamAppender;
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import static java.lang.invoke.MethodHandles.lookup;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import static org.slf4j.LoggerFactory.getLogger;
+
+
+/**
+ * An appender for buffering records.
+ *
+ * @author Jin Kwon &lt;jinahya_at_gmail.com&gt;
+ * @param <E> event type parameter
+ */
+public class BufferedOutputStreamAppender<E> extends OutputStreamAppender<E> {
+
+
+//    public BufferedOutputStreamAppender() {
+//
+//        super();
+//
+////        setOutputStream(buffer);
+//    }
+    @Override
+    public void start() {
+
+        setOutputStream(buffer);
+
+//        // oh, i can't help but doin' this.
+//        try {
+//            final Method method
+//                = OutputStreamAppender.class.getDeclaredMethod("encoderInit");
+//            if (!method.isAccessible()) {
+//                method.setAccessible(true);
+//            }
+//            method.invoke(this);
+//        } catch (final ReflectiveOperationException roe) {
+//            logger.error("failed to invoke encoderInit", roe);
+//        }
+
+        super.start();
+    }
+
+
+    @Override
+    protected void append(final E eventObject) {
+
+        super.append(eventObject);
+
+        // there is no other way to check
+        // whether eventObject actaully appended or not.
+        if (!isStarted()) {
+            return;
+        }
+
+        final byte[] record = buffer.toByteArray();
+        buffer.reset();
+        records.add(record);
+        length += record.length;
+
+        if (limit >= 0) {
+            while (length > limit && !records.isEmpty()) {
+                length -= records.remove(0).length;
+            }
+        }
+    }
+
+
+    /**
+     * Writes buffered records to specified {@code otuput}.
+     *
+     * @param output the output stream to which buffered records are written.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public void write(final OutputStream output) throws IOException {
+
+        for (final byte[] record : records) {
+            output.write(record);
+        }
+    }
+
+
+    /**
+     * Returns a concatenated buffered records.
+     *
+     * @return a concatenated buffered records.
+     */
+    public byte[] toByteArray() {
+
+        final byte[] array = new byte[length];
+
+        int position = 0;
+        for (final byte[] record : records) {
+            System.arraycopy(record, 0, array, position, record.length);
+            position += record.length;
+        }
+
+        return array;
+    }
+
+
+    /**
+     * Returns a String representing the buffered records.
+     *
+     * @return a String representing of buffered records.
+     *
+     * @see #toString(java.nio.charset.Charset)
+     */
+    @Override
+    public String toString() {
+
+        Charset charset = null;
+        if (encoder != null && encoder instanceof LayoutWrappingEncoder) {
+            charset = ((LayoutWrappingEncoder) encoder).getCharset();
+        }
+
+        return toString(charset);
+    }
+
+
+    /**
+     * Returns a string representing buffered records.
+     *
+     * @param charset the charset to encode.
+     *
+     * @return a string representing buffered records.
+     *
+     * @see #toByteArray()
+     */
+    public String toString(final Charset charset) {
+
+        final byte[] bytes = toByteArray();
+
+        if (charset == null) {
+            return new String(bytes);
+        }
+
+        return new String(bytes, charset);
+    }
+
+
+    /**
+     * Returns current value of {@code limit}.
+     *
+     * @return current value of {@code limit}.
+     */
+    public int getLimit() {
+
+        return limit;
+    }
+
+
+    /**
+     * Sets a new value for {@code limit}.
+     *
+     * @param limit new value of {@code limit}. Negative value for no limit.
+     */
+    public void setLimit(final int limit) {
+
+        this.limit = limit;
+    }
+
+
+    /**
+     * Returns the number of bytes buffered so far.
+     *
+     * @return the number of bytes buffered so far.
+     */
+    public int getLength() {
+
+        return length;
+    }
+
+
+    /**
+     * Resets buffered records and length.
+     */
+    public void reset() {
+
+        records.clear();
+        length = 0;
+    }
+
+
+    /**
+     * logger.
+     */
+    private transient final Logger logger = getLogger(lookup().lookupClass());
+
+
+    /**
+     * the output stream to buffer.
+     */
+    private final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+
+    /**
+     * the maximum number of bytes to buffer.
+     */
+    private int limit = 8192;
+
+
+    /**
+     * the buffered record list.
+     */
+    private final List<byte[]> records = new ArrayList<>();
+
+
+    /**
+     * the number of bytes buffered so far.
+     */
+    private int length = 0;
+
+
+}
+
---
+++ src/test/java/com/github/jinahya/logback/core/BufferedOutputStreamAppenderTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2015 Jin Kwon &lt;jinahya_at_gmail.com&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.github.jinahya.logback.core;
+
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.util.ContextSelectorStaticBinder;
+import static java.lang.invoke.MethodHandles.lookup;
+import java.nio.charset.StandardCharsets;
+import static java.util.concurrent.ThreadLocalRandom.current;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static org.slf4j.LoggerFactory.getLogger;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author Jin Kwon &lt;jinahya_at_gmail.com&gt;
+ */
+public class BufferedOutputStreamAppenderTest {
+
+
+    @Test(invocationCount = 128)
+    public void test() {
+
+        final LoggerContext context;
+        switch (current().nextInt(3)) {
+            case 0:
+                context = (LoggerContext) LoggerFactory.getILoggerFactory();
+                break;
+            case 1:
+                context = ContextSelectorStaticBinder.getSingleton()
+                    .getContextSelector().getDefaultLoggerContext();
+                break;
+            default:
+                context = ((ch.qos.logback.classic.Logger) logger)
+                    .getLoggerContext();
+                break;
+        }
+
+        final PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+        encoder.setContext(context);
+        encoder.setPattern("%msg%n");
+        encoder.setCharset(StandardCharsets.UTF_8);
+        encoder.start();
+
+        final BufferedOutputStreamAppender<ILoggingEvent> appender
+            = new BufferedOutputStreamAppender<>();
+        appender.setContext(context);
+        appender.setEncoder(encoder);
+        appender.start();
+
+        ((ch.qos.logback.classic.Logger) logger).addAppender(appender);
+
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn", new Exception("error"));
+        logger.error("error", new Exception("error"));
+
+        ((ch.qos.logback.classic.Logger) logger).detachAppender(appender);
+
+        appender.stop();
+
+        encoder.stop();
+
+        logger.debug("string: {}", appender.toString());
+        logger.debug("string: {}", appender.toString(encoder.getCharset()));
+    }
+
+
+    private transient final Logger logger = getLogger(lookup().lookupClass());
+
+
+}
+
